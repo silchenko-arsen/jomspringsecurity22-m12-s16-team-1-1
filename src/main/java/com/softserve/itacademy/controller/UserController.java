d1878709477dd1fcdccbe2b839d1a2c4a2083f7b
SashaShkirta <sandroall701@gmail.com>
2023-04-06 09:48:53 +0300

    Updated UserController

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -3,6 +3,7 @@ package com.softserve.itacademy.controller;
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.service.RoleService;
 import com.softserve.itacademy.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -39,6 +40,7 @@ public class UserController {
     }
 
     @GetMapping("/{id}/read")
+    @PreAuthorize("isAuthenticated()")
     public String read(@PathVariable long id, Model model) {
         User user = userService.readById(id);
         model.addAttribute("user", user);
@@ -46,6 +48,7 @@ public class UserController {
     }
 
     @GetMapping("/{id}/update")
+    @PreAuthorize("hasAuthority('ADMIN') or #ownerId=authentication.principal.id")
     public String update(@PathVariable long id, Model model) {
         User user = userService.readById(id);
         model.addAttribute("user", user);
@@ -55,6 +58,7 @@ public class UserController {
 
 
     @PostMapping("/{id}/update")
+    @PreAuthorize("hasAuthority('ADMIN') or #ownerId=authentication.principal.id")
     public String update(@PathVariable long id, Model model, @Validated @ModelAttribute("user") User user, @RequestParam("roleId") long roleId, BindingResult result) {
         User oldUser = userService.readById(id);
         if (result.hasErrors()) {
@@ -73,12 +77,14 @@ public class UserController {
 
 
     @GetMapping("/{id}/delete")
+    @PreAuthorize("hasAuthority('ADMIN') or #ownerId=authentication.principal.id")
     public String delete(@PathVariable("id") long id) {
         userService.delete(id);
         return "redirect:/users/all";
     }
 
     @GetMapping("/all")
+    @PreAuthorize("isAuthenticated()")
     public String getAll(Model model) {
         model.addAttribute("users", userService.getAll());
         return "users-list";
